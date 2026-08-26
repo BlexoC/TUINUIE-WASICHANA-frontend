62a01d9b82162b5da0907dcfa818394fd220da98
beatricemueni <muenisophie8@gmail.com>
2026-08-26 21:59:00 +0300

    chore: modify main React application entry file

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store";
+import { ToastProvider } from "./components/ToastContext";
+import { ErrorBoundary } from "./components/ErrorBoundary";
+import App from "./App.jsx";
+import "./index.css";
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <ToastProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ToastProvider>
+      </Provider>
+    </ErrorBoundary>
+  </StrictMode>
+);
